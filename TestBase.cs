e0ba6c25fefca5b5f3bf68608a5be1bcb97a33a9
Poornima Harsha <poornimanjesh@gmail.com>
2025-04-25 16:55:42 +0100

    PH_Commits added new class
    Refactor test structure with TestBase class
    
    Introduced the `TestBase` class to encapsulate common setup and teardown logic for Playwright tests. Removed the `TestLogin` class and integrated its functionality into `TestBase`. The `TestLoginAndLogOut` class now inherits from `TestBase`, promoting code reuse and simplifying the test structure.

---
+++ TestBase.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using NUnit.Framework;
+
+namespace commitQualityPractice
+{
+
+    public class TestBase
+    {
+        public IPage page; // Field to hold the IPage instance
+        public IBrowser browser; // Field to hold the browser instance
+        public IPlaywright playwright; // Field to hold the Playwright instance
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            playwright = await Playwright.CreateAsync();
+            browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false });
+            var context = await browser.NewContextAsync();
+            page = await context.NewPageAsync();
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            if (browser != null)
+            {
+                await browser.CloseAsync();
+            }
+            if (playwright != null)
+            {
+                playwright.Dispose();
+            }
+        }
+
+        [Test]
+        public async Task Test1()
+        {
+            // Navigating to the page
+            await page.GotoAsync(url: "http://eaapp.somee.com/");
+
+            await page.ClickAsync(selector: "text=Login");
+
+            // Screenshot
+            await page.ScreenshotAsync(new PageScreenshotOptions
+            {
+                Path = "loginpage.jpg"
+            });
+        }
+    }
+}
